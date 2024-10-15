e5db93ea9f234a4a4fea95a08de56c0e4e83297e
Maryna Sibova <maryna.i.kipenko@gmail.com>
2024-10-15 00:07:21 -0400

    solutions are added

---
+++ binarySearch.py
@@ -0,0 +1,28 @@
+def binary_search(arr, x):
+    if len(arr) < 1:
+        return -1
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+    attepms = 0
+ 
+    while low <= high:
+ 
+        mid = (high + low) // 2
+        attepms +=1
+        # якщо х більше ніж середне значення і менше ніж наступне за середнім - повертаємо більше за середне
+        if arr[mid] < x and x < arr[mid+1]:
+            return (attepms, arr[mid + 1])
+        # якщо х  дорівнює сер значенню або менше ніж середне значення і більше ніж попередне перед середнім - повертаємо меньше за середне        
+        elif x == arr[mid] or (arr[mid] > x and x > arr[mid-1]):
+            return (attepms, arr[mid])
+        
+        # якщо x більше за значення посередині списку, ігноруємо ліву половину
+        elif arr[mid] < x:
+            low = mid + 1
+ 
+        # якщо x менше за значення посередині списку, ігноруємо праву половину
+        elif arr[mid] > x:
+            high = mid - 1
+ 
+print(binary_search([1/2, 2/2, 3/2, 4/2], 2/3))
